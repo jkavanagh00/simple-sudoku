449c2d43f2036203752a216b4b85864eab888d2b
jkavanagh00 <jkavanagh00@gmail.com>
2025-10-19 17:42:56 +0200

    created method to check 3x3 boxes  => returns true if it is possible to add the number to the box, false if it is not

--- simple_sudoku.rb
+++ simple_sudoku.rb
@@ -23,3 +23,19 @@ def check_column(board, x_index, num)
         true
     end
 end
+
+def check_box(board, x_index, y_index, num)
+    box = []
+    top_left_x = (x_index / 3) * 3
+    top_left_y = (y_index / 3) * 3
+    for x in top_left_x..top_left_x + 2
+        for y in top_left_y..top_left_y + 2
+            box.push(board[y][x])
+        end
+    end
+    if box.include?(num)
+        false
+    else
+        true
+    end
+end
